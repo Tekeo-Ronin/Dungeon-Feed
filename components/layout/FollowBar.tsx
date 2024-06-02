1bdeb2d7884a5908ab68f2b912deec67abaa039a
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-06-02 23:36:01 +0200

    create follow bar

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -1,9 +1,29 @@
+import useUsers from "@/hooks/useUsers";
+
+import Avatar from "../Avatar";
+
 const FollowBar = () => {
+  const { data: users = [] } = useUsers();
+
+  if (users.length === 0) {
+    return null;
+  }
+
   return (
     <div className="hidden px-6 py-4 lg:block">
       <div className="rounded-xl bg-neutral-800 p-4">
-        <h2 className="font-semiblold text-xl text-white">Who to follow</h2>
-        <div className="mt-4 flex flex-col gap-6">{/* TODO USER LIST */}</div>
+        <h2 className="font-semiblold text-xl text-white">Who to follow:</h2>
+        <div className="mt-4 flex flex-col gap-6">
+          {users.map((user: Record<string, any>) => (
+            <div key={user.id} className="flex flex-row gap-4">
+              <Avatar userId={user.id} />
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-white">{user.name}</p>
+                <p className="text-sm text-neutral-400">@{user.username}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
