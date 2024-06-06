fb0152db93762eb4e3d21c55ac8c557a8994f4b6
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-06-06 09:23:29 +0200

    completed user page

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import useCurrrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
 import Button from "../Button";
+import { BiCalendar } from "react-icons/bi";
 
 interface UserBioPops {
   userId: string;
@@ -29,6 +30,31 @@ const UserBio: React.FC<UserBioPops> = ({ userId }) => {
         ) : (
           <Button onClick={() => {}} label="Follow" secondary />
         )}
+      </div>
+      <div className="mt-8 px-8">
+        <div className="flex flex-col">
+          <p className="text-2xl font-semibold text-white">
+            {fetchedUser?.name}
+          </p>
+          <p className="text-md text-neutral-500">@{fetchedUser?.username}</p>
+        </div>
+        <div className="mt-4 flex flex-col">
+          <p className="text-white">{fetchedUser?.bio}</p>
+          <div className="mt-4 flex flex-row items-center gap-2 text-neutral-500">
+            <BiCalendar size={24} />
+            <p>Joined {createdAt}</p>
+          </div>
+        </div>
+        <div className="mt-4 flex flex-row items-center gap-6">
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followingIds?.length}</p>
+            <p className="text-neutral-500"> Following</p>
+          </div>
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followersCount || 0}</p>
+            <p className="text-neutral-500"> Followers</p>
+          </div>
+        </div>
       </div>
     </div>
   );
