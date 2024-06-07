ab14708f4aa7a086d39a3a5d1199b02d2fdd91be
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-06-07 12:30:58 +0200

    add image edit features

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -8,6 +8,7 @@ import Modal from "../Modal";
 
 import { useCallback, useEffect, useState } from "react";
 import Input from "../Input";
+import ImageUpload from "../ImageUpload";
 
 const EditModal = () => {
   const { data: currentUser } = useCurrrentUser();
@@ -63,6 +64,18 @@ const EditModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
+      <ImageUpload
+        value={profileImage}
+        disabled={isLoading}
+        onChange={(image) => setProfileImage(image)}
+        label="Upload profile image"
+      />
+      <ImageUpload
+        value={coverImage}
+        disabled={isLoading}
+        onChange={(image) => setCoverImage(image)}
+        label="Upload cover image"
+      />
       <Input
         placeholder="Name"
         onChange={(e) => setName(e.target.value)}
