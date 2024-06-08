219e6c1f06b93a993f393d0b6c08208d59656d7e
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-06-08 10:50:20 +0200

    feat(Form.tsx): add post creation features

--- components/Button.tsx
+++ components/Button.tsx
@@ -24,14 +24,14 @@ const Button: React.FC<ButtonProps> = ({
       className={`rounded-full border-2 font-semibold transition hover:opacity-80
       disabled:cursor-not-allowed disabled:opacity-70 ${
         fullWidth ? "w-full" : "w-fit"
-      } ${secondary ? "bg-white" : "bg-sky-500"} ${
+      } ${secondary ? "bg-white" : "bg-rose-500"} ${
         secondary ? "text-black" : "text-white"
-      } ${secondary ? "border-black" : "border-sky-500"} ${
+      } ${secondary ? "border-black" : "border-rose-500"} ${
         large ? "text-xl" : "text-md"
       } ${large ? "px-5" : "px-4"} ${large ? "py-3" : "py-2"}
       ${outline ? "bg-transparent" : ""} ${outline ? "border-white" : ""} ${
         outline ? "text-white" : ""
-      } `}
+      }`}
     >
       {label}
     </button>
